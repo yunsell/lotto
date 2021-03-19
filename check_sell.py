8b4b15a457c8737dc9ae5e0f5dfe2297cc967cb1
yunsell <yunsell31@gmail.com>
2021-03-19 13:43:27 +0900

    process_time() 추가

--- check_sell.py
+++ check_sell.py
@@ -1,7 +1,9 @@
-import json
+import json, time
 from collections import Counter
 
 def check():
+    # start = time.time()
+    start = time.process_time()
     f = open('lotto.txt', 'r')
     lines = f.readlines()
     lotto_list = []
@@ -10,14 +12,15 @@ def check():
         lotto = json.loads(line.replace('\n', ''))
         for i in lotto:
             lotto_list.append(i)
-            count = Counter(lotto_list)
+    count = Counter(lotto_list)
 
     print('총',len(lines),'개의 번호를 분석했습니다.')
     print('*'*15)
     for k,v in count.most_common(n=6):
         print(k, '==>', v, '번')
     print('*'*15)
-
+    # print("걸린시간 :", time.time() - start)
+    print("걸린시간 :", time.process_time()-start, "초")
     # r = open('result.txt', 'a')
     # r.write(str(count))
     # r.close()
